470562fe4001e217d8935d3198985d3043d34aa9
Latha <lathanavaneeth@gmail.com>
2019-09-11 15:37:55 +1000

    initial commit

---
+++ src/components/reducers/cartReducer.js
@@ -0,0 +1,120 @@
+
+
+
+
+import Item1 from '../../images/item1.jpg'
+import Item2 from '../../images/item2.jpg'
+import Item3 from '../../images/item3.jpg'
+import Item4 from '../../images/item4.jpg'
+import Item5 from '../../images/item5.jpg'
+import Item6 from '../../images/item6.jpg'
+import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY,ADD_SHIPPING } from '../actions/action-types/cart-actions'
+
+const initState = {
+    items: [
+        {id:1,title:'Jute linen', desc: " Fall n pico done. Stitched blouse. Blouse size 38 with margin to extend.", price:110, img:Item1,},
+        {id:2,title:'German silver', desc: "Beautiful German silver set.", price:30,img: Item2},
+        {id:3,title:'chiffon Saree', desc: "Beautiful chiffon/nazmin Saree. Fall n pico done. Stitched blouse. Blouse size 38 with margin to extend.",price:80,img: Item3},
+        {id:4,title:'kids pavada / Lahanga sets ', desc: "Teen kids pavada / Lahanga sets for the festival season.Available size and price marked in the comments. These are size 38 and 40", price:20,img:Item4},
+        {id:5,title:'rangoli', desc: "rangolis", price:20,img: Item5},
+        {id:6,title:'Love for Terracota', desc: "Love for earth... love for terracota. These eco friendly sets are all ready to grace ur vanity. ",price:40,img: Item6}
+    ],
+    addedItems:[],
+    total: 0
+
+}
+const cartReducer= (state = initState,action)=>{
+   
+    //INSIDE HOME COMPONENT
+    if(action.type === ADD_TO_CART){
+          let addedItem = state.items.find(item=> item.id === action.id)
+          //check if the action id exists in the addedItems
+         let existed_item= state.addedItems.find(item=> action.id === item.id)
+         if(existed_item)
+         {
+            addedItem.quantity += 1 
+             return{
+                ...state,
+                 total: state.total + addedItem.price 
+                  }
+        }
+         else{
+            addedItem.quantity = 1;
+            //calculating the total
+            let newTotal = state.total + addedItem.price 
+            
+            return{
+                ...state,
+                addedItems: [...state.addedItems, addedItem],
+                total : newTotal
+            }
+            
+        }
+    }
+    if(action.type === REMOVE_ITEM){
+        let itemToRemove= state.addedItems.find(item=> action.id === item.id)
+        let new_items = state.addedItems.filter(item=> action.id !== item.id)
+        
+        //calculating the total
+        let newTotal = state.total - (itemToRemove.price * itemToRemove.quantity )
+        console.log(itemToRemove)
+        return{
+            ...state,
+            addedItems: new_items,
+            total: newTotal
+        }
+    }
+    //INSIDE CART COMPONENT
+    if(action.type=== ADD_QUANTITY){
+        let addedItem = state.items.find(item=> item.id === action.id)
+          addedItem.quantity += 1 
+          let newTotal = state.total + addedItem.price
+          return{
+              ...state,
+              total: newTotal
+          }
+    }
+    if(action.type=== SUB_QUANTITY){  
+        let addedItem = state.items.find(item=> item.id === action.id) 
+        //if the qt == 0 then it should be removed
+        if(addedItem.quantity === 1){
+            let new_items = state.addedItems.filter(item=>item.id !== action.id)
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                addedItems: new_items,
+                total: newTotal
+            }
+        }
+        else {
+            addedItem.quantity -= 1
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                total: newTotal
+            }
+        }
+        
+    }
+
+    if(action.type=== ADD_SHIPPING){
+          return{
+              ...state,
+              total: state.total + 6
+          }
+    }
+
+    if(action.type=== 'SUB_SHIPPING'){
+        return{
+            ...state,
+            total: state.total - 6
+        }
+  }
+    
+  else{
+    return state
+    }
+    
+}
+
+export default cartReducer
